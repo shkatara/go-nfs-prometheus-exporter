513c86a293a000f033638d23910f571dac66cb50
Shubham Katara <shubham.katara@trivago.com>
2024-04-27 21:47:27 +0200

    Add default labels in gauge metrics

--- main.go
+++ main.go
@@ -11,29 +11,24 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-func recordMetrics(size int64) {
-	byteSize.Set(float64(size))
-}
-
 var (
-	byteSize = promauto.NewGauge(prometheus.GaugeOpts{
-		Name: "dir_size_bytes",
-		Help: "The size of the directory in bytes",
+	gauge = promauto.NewGauge(prometheus.GaugeOpts{
+		Name:        "dir_size_bytes",
+		Help:        "The size of the directory in bytes",
+		ConstLabels: prometheus.Labels{"job": "go-nfs-exporter"},
 	})
 )
 
 func main() {
 	var target string = "./"
-	fmt.Println("Server started on port 8000")
 	http.Handle("/metrics", promhttp.Handler())
 	go http.ListenAndServe(":8000", nil)
-
 	dir := utils.FindDir(target)
 	for {
 		time.Sleep(5 * time.Second)
 		for _, data := range dir {
 			size := utils.DirSize(data)
-			recordMetrics(size)
+			gauge.Set(float64(size))
 			fmt.Println(size)
 		}
 	}
